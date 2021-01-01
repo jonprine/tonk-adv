cdfefdcd257014a910dfbf7cc41510b79a949a11
Vox Humana <jonprine@gmail.com>
2021-01-01 14:57:29 -0600

    worked on edit state

--- app.js
+++ app.js
@@ -105,6 +105,22 @@ const ItemCtrl = (function () {
 
       return newItem;
     },
+    getItemById: function(id) {
+      let found = null;
+      // loop through items
+      data.items.forEach(function(item) {
+        if(item.id === id){
+          found = item;
+        }
+      });
+      return found;
+    },
+    setCurrentItem: function(item) {
+      data.currentItem = item;
+    },
+    getCurrentItem: function() {
+      return data.currentItem;
+    },
     logData: function () {
       return data;
     },
@@ -228,12 +244,32 @@ const UICtrl = (function () {
         document.querySelector(UISelectors.itemShowtime).value = '';
         document.querySelector(UISelectors.itemArrival).value = '';
     },
+    addItemToForm: function() {
+      document.querySelector(UISelectors.itemDate).value = ItemCtrl.getCurrentItem().date;
+      document.querySelector(UISelectors.itemCity).value = ItemCtrl.getCurrentItem().city;
+      document.querySelector(UISelectors.itemVenue).value = ItemCtrl.getCurrentItem().venue;
+      document.querySelector(UISelectors.itemAddress).value = ItemCtrl.getCurrentItem().address;
+      document.querySelector(UISelectors.itemContact).value = ItemCtrl.getCurrentItem().contactname;
+      document.querySelector(UISelectors.itemPhone).value = ItemCtrl.getCurrentItem().phonenumber;
+      document.querySelector(UISelectors.itemEmail).value = ItemCtrl.getCurrentItem().email;
+      document.querySelector(UISelectors.itemDeal).value = ItemCtrl.getCurrentItem().deal;
+      document.querySelector(UISelectors.itemDeposit).value = ItemCtrl.getCurrentItem().deposit;
+      document.querySelector(UISelectors.itemShowtime).value = ItemCtrl.getCurrentItem().showtime;
+      document.querySelector(UISelectors.itemArrival).value = ItemCtrl.getCurrentItem().arrival;
+      UICtrl.showEditState();
+    },
     clearEditState: function () {
       UICtrl.clearInput();
       document.querySelector(UISelectors.updateBtn).style.display = 'none';
       document.querySelector(UISelectors.deleteBtn).style.display = 'none';
       document.querySelector(UISelectors.backBtn).style.display = 'none';
       document.querySelector(UISelectors.addBtn).style.display = 'inline';
+    },
+    showEditState: function () {
+      document.querySelector(UISelectors.updateBtn).style.display = 'inline';
+      document.querySelector(UISelectors.deleteBtn).style.display = 'inline';
+      document.querySelector(UISelectors.backBtn).style.display = 'inline';
+      document.querySelector(UISelectors.addBtn).style.display = 'none';
     },
     getSelectors: function () {
       return UISelectors;
@@ -252,7 +288,12 @@ const App = (function (ItemCtrl, UICtrl) {
     document
       .querySelector(UISelectors.addBtn)
       .addEventListener("click", itemAddSubmit);
+
+       // edit icon click event
+       document.querySelector(UISelectors.itemList).addEventListener("click", itemUpdateSubmit);
   };
+
+ 
 
   // add item submit
   const itemAddSubmit = function (e) {
@@ -297,6 +338,32 @@ const App = (function (ItemCtrl, UICtrl) {
 
     e.preventDefault();
   };
+
+  // update item submit
+  const itemUpdateSubmit = function(e) {
+    if(e.target.classList.contains('edit-item')) {
+      // get list item id (item-0, item-1)
+      const listId = e.target.parentNode.parentNode.id;
+
+      // break into an array
+      const listIdArr = listId.split('-');
+
+      // get the actual id
+      const id = parseInt(listIdArr[1]);
+
+      // get item
+      const itemToEdit = ItemCtrl.getItemById(id);
+
+      // set current item
+      ItemCtrl.setCurrentItem(itemToEdit);
+
+      // add item to form
+      UICtrl.addItemToForm();
+
+    }
+
+    e.preventDefault();
+  }
   // public meothods
   return {
     init: function () {
